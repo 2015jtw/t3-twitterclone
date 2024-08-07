3bbadefbe638588ed29fb918fab8c5786ccf8c69
Josh Worden <2015JTW@gmail.com>
2024-08-07 18:25:40 -0400

    clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ export default async function HomePage() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.createdAt),
   });
-  console.log("posts", images);
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
@@ -17,7 +16,6 @@ export default async function HomePage() {
           </div>
         ))}
       </div>
-      <h1>TEST...</h1>
     </main>
   );
 }
