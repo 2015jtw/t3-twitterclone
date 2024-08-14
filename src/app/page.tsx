2537f71966fa2353959e92c6c3fa9e71327229c1
Josh Worden <2015JTW@gmail.com>
2024-08-14 16:05:22 -0400

    set up image upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ async function Images() {
   });
   return (
     <div className="flex flex-wrap gap-4">
-      {[...images, ...images, ...images].map((image, idx) => (
-        <div key={image.id + "-" + idx} className="flex w-48 flex-col p-4">
+      {images.map((image) => (
+        <div key={image.id} className="flex w-48 flex-col p-4">
           <img src={image.url} alt="image" />
           <div>{image.name}</div>
         </div>
--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,14 +1,25 @@
+"use client";
+import { useRouter } from "next/navigation";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { UploadButton } from "~/utils/uploadthing";
 
 export default function TopNav() {
+  const router = useRouter();
+
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Gallery</div>
-      <div>
+      <div className="flex flex-row">
         <SignedOut>
           <SignInButton>Sign In</SignInButton>
         </SignedOut>
         <SignedIn>
+          <UploadButton
+            endpoint="imageUploader"
+            onClientUploadComplete={() => {
+              router.refresh();
+            }}
+          />
           <UserButton>Sign Out</UserButton>
         </SignedIn>
       </div>
