16164a999a825a37deb6418bf5e2b2fc65765c00
Josh Worden <2015JTW@gmail.com>
2024-09-23 09:41:06 +0200

    Setting up photo modal and getImage query

--- src/app/@modal/(.)photos/[id]/page.tsx
+++ src/app/@modal/(.)photos/[id]/page.tsx
@@ -1,9 +1,13 @@
-// import { Modal } from './modal';
+import { getImage } from "~/server/queries";
+import Image from "next/image";
 
-export default function PhotoModal({
+export default async function PhotoModal({
   params: { id: photoId },
 }: {
   params: { id: string };
 }) {
-  return <div>{photoId}</div>;
+  const idAsNum = Number(photoId);
+  if (Number.isNaN(idAsNum)) throw new Error("Invalid id");
+  const image = await getImage(idAsNum);
+  return <Image src={image.url} alt={image.name} width={192} height={192} />;
 }
