cf313ba8fb3c10efa821d8c396120b4442820ab9
Josh Worden <2015JTW@gmail.com>
2024-09-24 11:27:43 +0200

    finalized parallel routes and modal

--- src/components/FullPageImageView.tsx
+++ src/components/FullPageImageView.tsx
@@ -1,9 +1,10 @@
 import { getImage } from "~/server/queries";
+import { clerkClient } from "@clerk/nextjs/server";
 import Image from "next/image";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
-  console.log(image);
+  const uploaderInfo = await clerkClient.users.getUser(image.userId);
 
   return (
     <div className="flex h-full w-full min-w-0">
@@ -15,7 +16,15 @@ export default async function FullPageImageView(props: { id: number }) {
         />
       </div>
       <div className="flex w-48 flex-shrink-0 flex-col border-l">
-        <div className="text-xl font-bold">{image.name}</div>
+        <div className="border-b p-2 text-center text-xl">{image.name}</div>
+        <div className="flex flex-col p-2">
+          <span>Uploaded by </span>
+          <span>{uploaderInfo.fullName}</span>
+        </div>
+        <div className="flex flex-col p-2">
+          <span>Created on </span>
+          <span>{new Date(image.createdAt).toLocaleDateString()}</span>
+        </div>
       </div>
     </div>
   );
--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -2,13 +2,16 @@
 import { useRouter } from "next/navigation";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { UploadButton } from "~/utils/uploadthing";
+import Link from "next/link";
 
 export default function TopNav() {
   const router = useRouter();
 
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
-      <div>Gallery</div>
+      <div>
+        <Link href="/">Gallery</Link>
+      </div>
       <div className="flex flex-row">
         <SignedOut>
           <SignInButton>Sign In</SignInButton>
