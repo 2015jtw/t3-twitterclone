cb072b25f2333a8125cd3cd28c71319f69bb6c08
Josh Worden <2015JTW@gmail.com>
2024-10-01 17:40:42 +0200

    Improving upload button

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,28 +1,19 @@
-"use client";
-import { useRouter } from "next/navigation";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { UploadButton } from "~/utils/uploadthing";
 import Link from "next/link";
+import { SimpleUploadButton } from "./SimpleUploadButton";
 
 export default function TopNav() {
-  const router = useRouter();
-
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>
         <Link href="/">Gallery</Link>
       </div>
-      <div className="flex flex-row">
+      <div className="flex flex-row items-center gap-4">
         <SignedOut>
           <SignInButton>Sign In</SignInButton>
         </SignedOut>
         <SignedIn>
-          <UploadButton
-            endpoint="imageUploader"
-            onClientUploadComplete={() => {
-              router.refresh();
-            }}
-          />
+          <SimpleUploadButton />
           <UserButton />
         </SignedIn>
       </div>
